90f4ab4ffaf9c8d9cb935f3a1b1b1bb1a262d29c
Suyash Wagade <suyashwagade@gmail.com>
2026-01-01 10:15:33 +0530

    Varbiles with sum is added

--- 02 Varibales and Datatypes/Varibles.java
+++ 02 Varibales and Datatypes/Varibles.java
@@ -1,5 +1,26 @@
 
+// //Byte data type example in Java
+// class Codechef {
+//     public static void main(String[] args) {
+//         byte smallNumber = 50; // Declaring and assigning a byte variable
+//         System.out.println("The value of smallNumber is: " + smallNumber);
+//     }
+// }
 
+
+class Varibles {
+public static void main(String[] args) {
+    System.out.print("THe sum of these number is: ");
+    int num1=5;
+    int num2=10;
+    int num3=15;
+    int num4=20;
+
+    int sum=num1+num2+num3+num4;
+
+    System.out.print(sum);
+}
+}
 
 
 
