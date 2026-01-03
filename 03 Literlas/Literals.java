a42d015d17503c5657688e3efe6963a3a7cb9503
Suyash Wagade <suyashwagade@gmail.com>
2026-01-03 13:58:22 +0530

    Literlas added and 02 pratice

---
+++ 03 Literlas/Literals.java
@@ -0,0 +1,16 @@
+
+public class Literals {
+    public static void main(String[] args) {
+       byte age =34;
+       int age2 = 56;
+       char ch='A';
+       float f1 = 5.6f;
+       double d1 = 3.456;
+       boolean a =true;
+       String str="Harry";
+       System.out.println(str);
+       
+       System.out.println("age: " + age);
+    }
+
+}
---
+++ src/com/company/CWH_ch2_Operatros.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class CWH_ch2_Operatros {
+    public static void main(String[] args) {
+        int a =4;
+        int b =6 +a;
+        
+    }
+}
