6c25c76ddd2cc54b6fa03152cd13117c621d0b0c
Suyash Wagade <suyashwagade@gmail.com>
2026-01-04 13:04:11 +0530

    opreators learning started

--- src/com/company/CWH_ch2_Operatros.java
+++ src/com/company/CWH_ch2_Operatros.java
@@ -3,7 +3,8 @@ package com.company;
 public class CWH_ch2_Operatros {
     public static void main(String[] args) {
         int a =4;
-        int b =6 +a;
+        int b =6 * a;
+        System.out.println(b);
         
     }
 }
