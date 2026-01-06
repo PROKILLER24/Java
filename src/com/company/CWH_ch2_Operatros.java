d9afb2c58b578edaa5e10c3cfd866b65ea3a7d0d
Suyash Wagade <suyashwagade@gmail.com>
2026-01-06 22:17:08 +0530

    the operators basic added

--- src/com/company/CWH_ch2_Operatros.java
+++ src/com/company/CWH_ch2_Operatros.java
@@ -3,8 +3,11 @@ package com.company;
 public class CWH_ch2_Operatros {
     public static void main(String[] args) {
         int a =4;
-        int b =6 * a;
+        //int b =6 %a;
+        int b =9;
+        b +=3;
         System.out.println(b);
+        System.out.println(6==8);
         
     }
 }
